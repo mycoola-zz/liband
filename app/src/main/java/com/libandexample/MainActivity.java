8a0f9a08b4adb4046d2d6d30bb3f9db69eb968d8
michael.girgis <dev.michael.girgis@gmail.com>
2018-06-22 23:53:12 +0200

    - add drawablesUtils.

---
+++ app/src/main/java/com/libandexample/MainActivity.java
@@ -0,0 +1,21 @@
+package com.libandexample;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.liband.utils.DrawablesUtil;
+import com.libandexample.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding activityMainBinding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        DrawablesUtil.changeBackgroundDrawable(activityMainBinding.editText, getResources().getColor(android.R.color.transparent),
+                getResources().getColor(android.R.color.black), 10, 20);
+    }
+}
---
+++ liband/src/main/java/com/liband/utils/DrawablesUtil.java
@@ -0,0 +1,18 @@
+package com.liband.utils;
+
+import android.graphics.drawable.GradientDrawable;
+import android.support.v7.widget.DrawableUtils;
+import android.view.View;
+
+public class DrawablesUtil {
+
+    public static void changeBackgroundDrawable(View view, int solidColor, int strokeColor, int strokeWidth, float radiusNumber) {
+
+        GradientDrawable drawable = (GradientDrawable) view.getBackground();
+        drawable.setColor(solidColor);
+        drawable.setStroke(strokeWidth, strokeColor);
+        drawable.setCornerRadius(radiusNumber);
+
+    }
+
+}
