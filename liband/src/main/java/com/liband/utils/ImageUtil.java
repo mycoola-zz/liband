4b75c0e30e00e99319c03afb5b8580e40337a836
michael.girgis <dev.michael.girgis@gmail.com>
2018-06-24 19:56:14 +0200

    - add empty image. - change imagutils method.

--- liband/src/main/java/com/liband/utils/ImageUtil.java
+++ liband/src/main/java/com/liband/utils/ImageUtil.java
@@ -7,40 +7,40 @@ import android.support.v7.widget.AppCompatImageView;
 import android.text.TextUtils;
 import android.view.View;
 
+import com.liband.R;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
 
 public class ImageUtil {
 
-    public static void loadImageFromPicasso(final Context activity, final String imageUrl, final AppCompatImageView imageView, final AppCompatImageView placeholder) {
+    public static void loadImageFromPicasso(final Context activity, final String imageUrl,
+                                            final AppCompatImageView imageView) {
 
         if (!TextUtils.isEmpty(imageUrl)) {
             final Target target = new Target() {
                 @Override
                 public void onPrepareLoad(Drawable arg0) {
-                    placeholder.setVisibility(View.VISIBLE);
+                    imageView.setBackgroundDrawable(activity.getDrawable(R.drawable.empty_image));
                 }
 
                 @Override
                 public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom arg1) {
                     if (bitmap != null) {
-                        //imageView.setBackground(new BitmapDrawable(activity.getResources(), bitmap));
                         imageView.setImageBitmap(bitmap);
-                        placeholder.setVisibility(View.GONE);
                     }
                 }
 
                 @Override
                 public void onBitmapFailed(Drawable arg0) {
-                    placeholder.setVisibility(View.VISIBLE);
+                    imageView.setBackgroundDrawable(activity.getDrawable(R.drawable.empty_image));
                 }
             };
             imageView.setTag(target);
             Picasso.with(activity).load(imageUrl).
                     into(target);
         } else {
-            placeholder.setVisibility(View.VISIBLE);
+            imageView.setVisibility(View.VISIBLE);
         }
     }
 
