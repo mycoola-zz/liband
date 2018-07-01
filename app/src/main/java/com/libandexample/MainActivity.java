48d40a9021e39e8333578a9341fe8ff15ccce3de
michael.girgis <dev.michael.girgis@gmail.com>
2018-07-01 23:53:04 +0200

    - startanother activity from example.

--- app/src/main/java/com/libandexample/MainActivity.java
+++ app/src/main/java/com/libandexample/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.liband.BaseActivity;
+import com.liband.constants.TagsName;
 import com.liband.utils.DrawablesUtil;
 import com.libandexample.databinding.ActivityMainBinding;
 
@@ -18,6 +19,6 @@ public class MainActivity extends BaseActivity {
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         DrawablesUtil.changeBackgroundDrawable(activityMainBinding.editText, getResources().getColor(android.R.color.transparent),
                 getResources().getColor(android.R.color.black), 10, 20);
-        startActivity(Main2Activity.class);
+        startAnotherActivity(Main2Activity.class);
     }
 }
