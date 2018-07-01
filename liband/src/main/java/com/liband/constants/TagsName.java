29de2e71188e6548e060a14b8e50ffea89bf1092
michael.girgis <dev.michael.girgis@gmail.com>
2018-07-01 15:18:38 +0200

    - add date to tagsname.

---
+++ liband/src/main/java/com/liband/constants/TagsName.java
@@ -0,0 +1,32 @@
+package com.liband.constants;
+
+
+class TagsName {
+    String ID = "id";
+    String TITLE = "title";
+    String NAME = "name";
+    String DETAILS = "details";
+    String THUMBNAIL = "thumbnail";
+    String BANNER = "banner";
+    String DESCRIPTION = "description";
+    String IMAGE = "image";
+    String IMAGES = "images";
+    String CATEGORY = "category";
+    String IS_CELEBRITY = "is_celebrity";
+    String FEEDING_HOURS = "feeding_hours";
+    String AND = "&";
+    String EQUAL = "=";
+    String ITEMS_PER_PAGE = "items_per_page";
+    String PAGE = "page";
+    String LANGCODE = "langcode";
+    String CATEGORY_ID = "category_id";
+    String LATITUDE = "latitude";
+    String LONGITUDE = "longitude";
+    String START_DATE = "start_date";
+    String END_DATE = "end_date";
+    String DATES = "dates";
+    String OFFSET = "offset";
+    String LIMIT = "limit";
+
+
+}
