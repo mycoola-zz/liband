e06dacbff5e8443cc2e99a528e7758a23e3db46d
michael.girgis <dev.michael.girgis@gmail.com>
2018-07-01 15:39:55 +0200

    - set tagsName as public with static data.

--- liband/src/main/java/com/liband/constants/TagsName.java
+++ liband/src/main/java/com/liband/constants/TagsName.java
@@ -1,32 +1,32 @@
 package com.liband.constants;
 
 
-class TagsName {
-    String ID = "id";
-    String TITLE = "title";
-    String NAME = "name";
-    String DETAILS = "details";
-    String THUMBNAIL = "thumbnail";
-    String BANNER = "banner";
-    String DESCRIPTION = "description";
-    String IMAGE = "image";
-    String IMAGES = "images";
-    String CATEGORY = "category";
-    String IS_CELEBRITY = "is_celebrity";
-    String FEEDING_HOURS = "feeding_hours";
-    String AND = "&";
-    String EQUAL = "=";
-    String ITEMS_PER_PAGE = "items_per_page";
-    String PAGE = "page";
-    String LANGCODE = "langcode";
-    String CATEGORY_ID = "category_id";
-    String LATITUDE = "latitude";
-    String LONGITUDE = "longitude";
-    String START_DATE = "start_date";
-    String END_DATE = "end_date";
-    String DATES = "dates";
-    String OFFSET = "offset";
-    String LIMIT = "limit";
+public class TagsName {
+    public static final String ID = "id";
+    public static final String TITLE = "title";
+    public static final String NAME = "name";
+    public static final String DETAILS = "details";
+    public static final String THUMBNAIL = "thumbnail";
+    public static final String BANNER = "banner";
+    public static final String DESCRIPTION = "description";
+    public static final String IMAGE = "image";
+    public static final String IMAGES = "images";
+    public static final String CATEGORY = "category";
+    public static final String IS_CELEBRITY = "is_celebrity";
+    public static final String FEEDING_HOURS = "feeding_hours";
+    public static final String AND = "&";
+    public static final String EQUAL = "=";
+    public static final String ITEMS_PER_PAGE = "items_per_page";
+    public static final String PAGE = "page";
+    public static final String LANGCODE = "langcode";
+    public static final String CATEGORY_ID = "category_id";
+    public static final String LATITUDE = "latitude";
+    public static final String LONGITUDE = "longitude";
+    public static final String START_DATE = "start_date";
+    public static final String END_DATE = "end_date";
+    public static final String DATES = "dates";
+    public static final String OFFSET = "offset";
+    public static final String LIMIT = "limit";
 
 
 }
