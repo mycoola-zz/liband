f4dd73a7434a9f1c5f614694aa3f2503da77afb4
Michael Girgis <dev.michael.girgis@gmail.com>
2018-07-28 20:27:05 +0200

    - add method reflect visability of view in utils class

--- liband/src/main/java/com/liband/utils/Utils.java
+++ liband/src/main/java/com/liband/utils/Utils.java
@@ -2,6 +2,7 @@ package com.liband.utils;
 
 import android.content.res.Resources;
 import android.util.TypedValue;
+import android.view.View;
 
 import com.liband.App;
 
@@ -13,5 +14,9 @@ public class Utils {
     public static int dpToPx(int dp) {
         Resources r = App.getApp().getResources();
         return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+
+    public static void reflectVisibilityOfView(View view) {
+        view.setVisibility(view.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
     }
 }
