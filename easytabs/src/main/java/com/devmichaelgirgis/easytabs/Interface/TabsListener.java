a5b17ebc945db664070213ea7b47561a783e764c
Michael Girgis <dev.michael.girgis@gmail.com>
2018-09-25 20:34:42 +0200

    -delete easytabs.

--- easytabs/src/main/java/com/devmichaelgirgis/easytabs/Interface/TabsListener.java
+++
@@ -1,9 +0,0 @@
-package com.devmichaelgirgis.easytabs.Interface;
-
-/**
- * Created by gilgoldzweig on 08/09/2016.
- */
-
-public interface TabsListener {
-    void onScreenPosition(int position);
-}
